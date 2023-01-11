ebc65757a61fa1b0fe8d340c5e69fe9d75d0c7a3
KirSivogr <kirillsivograkovv@mail.ru>
2023-01-11 20:18:45 +0300

    add main javafx

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+    static String key = "sadadsa";
+    static String inputTxt = "__fixtures__/input.txt";
+    static String outputTxt = "__fixtures__/output.txt";
+    static String inputXml = "__fixtures__/input.xml";
+    static String outputXml = "__fixtures__/output.xml";
+    static String encryptedOutputTxt = "__fixtures__/encryptedOutput.txt";
+    static String outputZip = "__fixtures__/output.zip";
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
