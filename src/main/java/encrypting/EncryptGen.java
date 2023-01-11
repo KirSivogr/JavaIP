66cdda0bf0278821144153e97c593843ff628154
KirSivogr <kirillsivograkovv@mail.ru>
2023-01-11 20:19:04 +0300

    add archiving

---
+++ src/main/java/encrypting/EncryptGen.java
@@ -0,0 +1,63 @@
+package encrypting;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class EncryptGen {
+    static private byte[] KEYVALUE = "6^)(9-p35@%3#4S!4S0)$Y%%^&5(j.&^&o(*0)$Y%!#O@*GpG@=+@j.&6^)(0-=+".getBytes();;
+    static final int BUFFERLEN = 1024;
+
+    public EncryptGen() {};
+
+    public EncryptGen(String key) {
+        KEYVALUE = key.getBytes();
+    }
+
+    public static void EncryptFile(String oldFile, String newFile) throws Exception {
+        FileInputStream in = new FileInputStream(oldFile);
+        File file = new File(newFile);
+        if (!file.exists())
+            file.createNewFile();
+        FileOutputStream out = new FileOutputStream(file);
+        int c, pos, keylen;
+        pos = 0;
+        keylen = KEYVALUE.length;
+        byte buffer[] = new byte[BUFFERLEN];
+        while ((c = in.read(buffer)) != -1) {
+            for (int i = 0; i < c; i++) {
+                buffer[i] ^= KEYVALUE[pos];
+                out.write(buffer[i]);
+                pos++;
+                if (pos == keylen)
+                    pos = 0;
+            }
+        }
+        in.close();
+        out.close();
+    }
+
+    public static void DecryptFile(String oldFile, String newFile) throws Exception {
+        FileInputStream in = new FileInputStream(oldFile);
+        File file = new File(newFile);
+        if (!file.exists())
+            file.createNewFile();
+        FileOutputStream out = new FileOutputStream(file);
+        int c, pos, keylen;
+        pos = 0;
+        keylen = KEYVALUE.length;
+        byte buffer[] = new byte[BUFFERLEN];
+        while ((c = in.read(buffer)) != -1) {
+            for (int i = 0; i < c; i++) {
+                buffer[i] ^= KEYVALUE[pos];
+                out.write(buffer[i]);
+                pos++;
+                if (pos == keylen)
+                    pos = 0;
+            }
+        }
+        in.close();
+        out.close();
+    }
+};
+
